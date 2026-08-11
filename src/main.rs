06229f587ecab31a5ec4d51def173a39ff044b1e
asahi kojima <ubutara12@gmail.com>
2026-08-11 16:29:15 +0900

    テストの削除

--- src/main.rs
+++ src/main.rs
@@ -27,23 +27,3 @@ fn main()
     let mut app = App::new(renderer, presenter, camera, 30);
     app.run().unwrap();
 }
-
-
-#[cfg(test)]
-mod test
-{
-    use super::*;
-
-    #[test]
-    pub fn test_transform()
-    {
-        let transform = Transform::identity()
-            .with_position(Point::new(1.0, 2.0, 3.0))
-            .with_rotation(Quaternion::try_from_axis_angle(Vec3::unit_y(), 90.0_f64.to_radians()).unwrap())
-            .with_scale(Vec3::new(2.0, 2.0, 2.0));
-
-        assert!(transform.position() == Point::new(1.0, 2.0, 3.0));
-        assert!(transform.rotation() == Quaternion::try_from_axis_angle(Vec3::unit_y(), 90.0_f64.to_radians()).unwrap());
-        assert!(transform.scale() == Vec3::new(2.0, 2.0, 2.0));
-    }
-}
