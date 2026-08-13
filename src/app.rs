57129e54a0ced9e27bfb1d5983579aff03518d73
asahi kojima <ubutara12@gmail.com>
2026-08-14 01:19:25 +0900

    FPSの正確な制御

--- src/app.rs
+++ src/app.rs
@@ -9,8 +9,23 @@ use crate::platform::Presenter;
 use crate::render::{Frame, RenderContext, Renderer};
 use crate::scene::Vertex;
 
-const MOVE_SPEED: f64 = 0.1;
+const MOVE_SPEED: f64 = 1.0;
 const ROT_SPEED_RAD: f64 = 3.0_f64.to_radians();
+
+
+#[derive(Debug, Clone, Copy, Default)]
+struct InputState
+{
+    pub is_w_pressed: bool,
+    pub is_s_pressed: bool,
+    pub is_a_pressed: bool,
+    pub is_d_pressed: bool,
+
+    pub is_right_pressed: bool,
+    pub is_left_pressed : bool,
+    pub is_up_pressed   : bool,
+    pub is_down_pressed : bool,
+}
 
 
 pub struct App
@@ -58,20 +73,32 @@ impl App
         // 開始時刻
         let start = Instant::now();
 
-        // このフレームの終了時刻
-        let mut frame_finish_time = Instant::now();
- 
+        let target_frame_duration = Duration::from_secs_f64(1.0 / self.fps as f64);
+
         // 前フレームからの経過時間
         let mut delta_time = Duration::from_secs(0);
-
+        
         // このフレームの開始時刻
         let mut frame_start_time = Instant::now();
 
         // 開始時点からの経過フレーム
         let mut frame_index = 0_u64;
 
+        let mut input_state = InputState::default();
+
         loop
         {
+            // ----------------------------------------------------------------
+            // 時間の計測と更新
+            // ----------------------------------------------------------------
+            let now = Instant::now();
+            delta_time = now.duration_since(frame_start_time);
+            frame_start_time = now;
+
+
+            // ----------------------------------------------------------------
+            // イベント処理
+            // ----------------------------------------------------------------
             let mut events = Vec::<InputEvent>::new();
             if self.presenter.handle_events(&mut events)
             {
@@ -87,16 +114,16 @@ impl App
                         match key
                         {
                             // WASD: 平行移動
-                            Key::W => self.camera.move_forward(MOVE_SPEED),
-                            Key::S => self.camera.move_forward(-MOVE_SPEED),
-                            Key::A => self.camera.move_right(-MOVE_SPEED),
-                            Key::D => self.camera.move_right(MOVE_SPEED),
+                            Key::W => input_state.is_w_pressed = true,
+                            Key::S => input_state.is_s_pressed = true,
+                            Key::A => input_state.is_a_pressed = true,
+                            Key::D => input_state.is_d_pressed = true,
                             
                             // 矢印: 視点回転
-                            Key::LEFT  => self.camera.yaw(ROT_SPEED_RAD),
-                            Key::RIGHT => self.camera.yaw(-ROT_SPEED_RAD),
-                            Key::UP    => self.camera.pitch(ROT_SPEED_RAD),
-                            Key::DOWN  => self.camera.pitch(-ROT_SPEED_RAD),
+                            Key::LEFT  => input_state.is_left_pressed  = true,
+                            Key::RIGHT => input_state.is_right_pressed = true,
+                            Key::UP    => input_state.is_up_pressed    = true,
+                            Key::DOWN  => input_state.is_down_pressed  = true,
 
                             // シーンの保存
                             Key::P => 
@@ -125,6 +152,26 @@ impl App
                             _ => {}
                         }
                     },
+                    InputEvent::KeyUp(key) =>
+                    {
+                        match key
+                        {
+
+                            // WASD: 平行移動
+                            Key::W => input_state.is_w_pressed = false,
+                            Key::S => input_state.is_s_pressed = false,
+                            Key::A => input_state.is_a_pressed = false,
+                            Key::D => input_state.is_d_pressed = false,
+                            
+                            // 矢印: 視点回転
+                            Key::LEFT  => input_state.is_left_pressed  = false,
+                            Key::RIGHT => input_state.is_right_pressed = false,
+                            Key::UP    => input_state.is_up_pressed    = false,
+                            Key::DOWN  => input_state.is_down_pressed  = false,
+
+                            _ => println!("no action"),
+                        }
+                    },
                     InputEvent::MouseMove { x, y, dx, dy } =>
                     {
                         // 例: 右ドラッグ中だけ視点回転に使うなど
@@ -149,7 +196,6 @@ impl App
                         println!("mouse wheel: ({x},{y})");
                         //TODO
                     },
-
                     InputEvent::TextInput(text) =>
                     {
                         println!("TextInput: {}", text);
@@ -159,13 +205,35 @@ impl App
                 }
             }
 
+            // ----------------------------------------------------------------
+            // カメラの移動
+            // ----------------------------------------------------------------
+            {
+                let effective_move_speed = MOVE_SPEED * delta_time.as_secs_f64();println!("{}", effective_move_speed);
+                let effective_rot_speed_rad = ROT_SPEED_RAD * delta_time.as_secs_f64();
+
+                if input_state.is_w_pressed { self.camera.move_forward(effective_move_speed); }
+                if input_state.is_s_pressed { self.camera.move_forward(-effective_move_speed); }
+                if input_state.is_a_pressed { self.camera.move_right(-effective_move_speed); }
+                if input_state.is_d_pressed { self.camera.move_right(effective_move_speed); }
+                
+                if input_state.is_left_pressed  { self.camera.yaw(effective_rot_speed_rad); }
+                if input_state.is_right_pressed { self.camera.yaw(-effective_rot_speed_rad); }
+                if input_state.is_up_pressed    { self.camera.pitch(effective_rot_speed_rad); }
+                if input_state.is_down_pressed  { self.camera.pitch(-effective_rot_speed_rad); }
+            }
+
+            // ----------------------------------------------------------------
             // レンダリングに必要な情報をまとめる
+            // ----------------------------------------------------------------
             let render_context = RenderContext {
                 frame_index,
                 elapsed_seconds: start.elapsed().as_secs_f32(),
             };
 
+            // ----------------------------------------------------------------
             // シーンが変更されていたら、ランタイムシーンを再コンパイルする
+            // ----------------------------------------------------------------
             if self.scene_dirty
             {
                 self.runtime_scene = self
@@ -175,7 +243,9 @@ impl App
                 self.scene_dirty = false;
             }
 
+            // ----------------------------------------------------------------
             // レンダリング開始
+            // ----------------------------------------------------------------
             self.renderer.render(
                 &mut self.render_target,
                 &self.camera,
@@ -183,11 +253,30 @@ impl App
                 &render_context,
             )?;
 
+            // ----------------------------------------------------------------
             // レンダリング結果を画面に表示する
+            // ----------------------------------------------------------------
             self.presenter.present(&self.render_target)?;
 
+            // ----------------------------------------------------------------
+            // FPSの制御
+            // ----------------------------------------------------------------
             frame_index += 1;
-            std::thread::sleep(Duration::from_millis(16));
+            let frame_work_duration = frame_start_time.elapsed();
+            if frame_work_duration < target_frame_duration
+            {
+                let sleep_time = target_frame_duration - frame_work_duration;
+
+                if sleep_time > Duration::from_millis(2)
+                {
+                    std::thread::sleep(sleep_time - Duration::from_millis(2));
+                }
+
+                while frame_start_time.elapsed() < target_frame_duration
+                {
+                    std::hint::spin_loop();
+                }
+            }
         }
     }
 
